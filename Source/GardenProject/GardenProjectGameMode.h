c510c5a9dd805a7618d6cbb3f44f705226f32a9f
Alexandre <alexandre.depail@gmail.com>
2021-12-03 12:12:52 +0100

    Initial commit

---
+++ Source/GardenProject/GardenProjectGameMode.h
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GardenProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGardenProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGardenProjectGameMode();
+};
